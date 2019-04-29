e475b1c9190b4eb124b9f9480b27f681dfb97926
SebinLee <lsbjeju@gmail.com>
2019-04-29 15:26:04 +0900

    Add 02.02 PushButton
    Add PushButton Code

---
+++ 02.02 PushButton/pushbuttonTest.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+#UI파일 연결
+#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+form_class = uic.loadUiType("pushbuttonTest.ui")[0]
+
+#화면을 띄우는데 사용되는 Class 선언
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+        self.btn_1.clicked.connect(self.button1Function)
+        self.btn_2.clicked.connect(self.button2Function)
+
+    def button1Function(self) :
+        print("btn_1 Clicked")
+
+    def button2Function(self) :
+        print("btn_2 Clicked")
+
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass() 
+    myWindow.show()
+    app.exec_()
