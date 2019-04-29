d1c07851b9b4697f2c5ed46698f824c0e973359c
SebinLee <lsbjeju@gmail.com>
2019-04-29 16:10:33 +0900

    Add 02.07 LineEdit
    Add 02.07 LineEdit Code

---
+++ 02.07 LineEdit/lineeditTest.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("lineeditTest.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+        #버튼에 기능을 할당하는 코드
+        self.lineedit_Test.textChanged.connect(self.lineeditTextFunction)
+        self.lineedit_Test.returnPressed.connect(self.printTextFunction)
+        self.btn_changeText.clicked.connect(self.changeTextFunction)
+
+    def lineeditTextFunction(self) :
+        self.lbl_textHere.setText(self.lineedit_Test.text())
+
+    def printTextFunction(self) :
+        #self.Label이름.text()
+        #Label에 있는 글자를 가져오는 메서드
+        print(self.lineedit_Test.text())
+
+    def changeTextFunction(self) :
+        #self.Label이름.setText("String")
+        #Label에 글자를 바꾸는 메서드
+        self.lineedit_Test.setText("Change Text")
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
