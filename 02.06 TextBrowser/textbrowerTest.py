02bdd374843724df657887e7336adafed9abd612
SebinLee <lsbjeju@gmail.com>
2019-04-29 15:57:59 +0900

    Add 02.06 Textbrower
    Add TextBrower COde

--- 02.06 TextBrowser/textbrowerTest.py
+++ 02.06 TextBrowser/textbrowerTest.py
@@ -2,14 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-form_class = 
-
-
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
-
-form_class = uic.loadUiType("labelTest.ui")[0]
+form_class = uic.loadUiType("textbrowserTest.ui")[0]
 
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
@@ -17,18 +10,30 @@ class WindowClass(QMainWindow, form_class) :
         self.setupUi(self)
 
         #버튼에 기능을 할당하는 코드
-        self.btn_changeText.clicked.connect(self.changeTextFunction)
-        self.btn_printText.clicked.connect(self.printTextFunction)
-
-    def changeTextFunction(self) :
-        #self.Label이름.setText("String")
-        #Label에 글자를 바꾸는 메서드
-        self.lbl_Test.setText("This is Label - Change Text")
+        self.btn_Print.clicked.connect(self.printTextFunction)
+        self.btn_setText.clicked.connect(self.changeTextFunction)
+        self.btn_appendText.clicked.connect(self.appendTextFunction)
+        self.btn_Clear.clicked.connect(self.clearTextFunction)
 
     def printTextFunction(self) :
-        #self.Label이름.text()
-        #Label에 있는 글자를 가져오는 메서드
-        print(self.lbl_Test.text())
+        #self.Textbrowser이름.toPlainText()
+        #Textbrowser에 있는 글자를 가져오는 메서드
+        print(self.textbrow_Test.toPlainText())
+
+    def changeTextFunction(self) :
+        #self.Textbrowser이름.setPlainText()
+        #Textbrowser에 있는 글자를 가져오는 메서드
+        self.textbrow_Test.setPlainText("This is Textbrowser - Change Text")
+
+    def appendTextFunction(self) :
+        #self.Textbrowser이름.append()
+        #Textbrowser에 있는 글자를 가져오는 메서드
+        self.textbrow_Test.append("Append Text")
+
+    def clearTextFunction(self) :
+        #self.Textbrowser.clear()
+        #Textbrowser에 있는 글자를 지우는 메서드
+        self.textbrow_Test.clear()
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
