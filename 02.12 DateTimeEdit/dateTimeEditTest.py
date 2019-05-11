1c6469f9cdfca4adc0e2da0c8bca8e8a9d0dc89d
SebinLee <lsbjeju@gmail.com>
2019-05-11 15:46:47 +0900

    Add 02.12 DateTime
    Add DateTime Code

---
+++ 02.12 DateTimeEdit/dateTimeEditTest.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("dateTimeEditTest.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+
+    """
+    QDateTimeEdit
+    - date()
+    - dateTime()
+
+    - clearMaximumDate, DateTime, Time
+    - clearMinumumDate, DateTime, Time
+    - currentSection()
+
+    """
+
+    def displayDateTime(self) :
+        self.datetimeedit_Test.dateTime()
+
+        
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_() 
