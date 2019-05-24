1c66813da5407978162b5accf065274f16439348
SebinLee <lsbjeju@gmail.com>
2019-05-24 23:28:57 +0900

    Final 02.15 Pixmap
    Final Edit of 02.15 Pixmap Example Code & UI FIle

--- 02.15 Pixmap/pixmapTest.py
+++ 02.15 Pixmap/pixmapTest.py
@@ -12,7 +12,12 @@ class WindowClass(QMainWindow, form_class) :
         super().__init__()
         self.setupUi(self)
 
+        self.btn_loadFromFile.clicked.connect(self.loadImageFromFile)
+        self.btn_loadFromWeb.clicked.connect(self.loadImageFromWeb)
+        self.btn_savePicture.clicked.connect(self.saveImageFromWeb)
+
     def loadImageFromFile(self) :
+        #QPixmap 객체 생성 후 이미지 파일을 이용하여 QPixmap에 사진 데이터 Load하고, Label을 이용하여 화면에 표시
         self.qPixmapFileVar = QPixmap()
         self.qPixmapFileVar.load("testImage.jpg")
         self.qPixmapFileVar = self.qPixmapFileVar.scaledToWidth(600)
@@ -24,11 +29,16 @@ class WindowClass(QMainWindow, form_class) :
         urlString = "https://avatars1.githubusercontent.com/u/44885477?s=460&v=4"
         imageFromWeb = urllib.request.urlopen(urlString).read()
 
-        #웹에서 Load한 Image를 QPixmap 객체로 만들고, Label을 이용하여 화면에 표시
+        #웹에서 Load한 Image를 이용하여 QPixmap에 사진데이터를 Load하고, Label을 이용하여 화면에 표시
         self.qPixmapWebVar = QPixmap()
         self.qPixmapWebVar.loadFromData(imageFromWeb)
         self.qPixmapWebVar = self.qPixmapWebVar.scaledToWidth(600)
         self.lbl_picture.setPixmap(self.qPixmapWebVar)
+
+    def saveImageFromWeb(self) :
+        #Label에서 표시하고 있는 사진 데이터를 QPixmap객체의 형태로 반환받은 후, save함수를 이용해 사진 저장
+        self.qPixmapSaveVar = self.lbl_picture.pixmap()
+        self.qPixmapSaveVar.save("SavedImage.jpg")
 
 
 if __name__ == "__main__" :
