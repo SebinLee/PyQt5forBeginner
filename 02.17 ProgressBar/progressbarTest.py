83869b36450865b063a11735130aa474e320fc79
SebinLee <lsbjeju@gmail.com>
2019-05-25 20:52:33 +0900

    Final 02.17 ProgressBar
    Final Edit 02.17 ProgessBar Example Code & UI File

--- 02.17 ProgressBar/progressbarTest.py
+++ 02.17 ProgressBar/progressbarTest.py
@@ -11,22 +11,29 @@ class WindowClass(QMainWindow, form_class) :
         super().__init__()
         self.setupUi(self)
 
-        """
-        #Signal
-        valueChanged
+        #ProgressBar의 시그널
+        self.progressBar_Test.valueChanged.connect(self.printValue)
 
-        #Function
-        maximum() / minimum()
-        setMaximum() / setMinimum()
-        setRange()
+        #QTimer를 이용하여 매초마다 ProgressBar의 값이 1씩 늘어나게 설정합니다.
+        #QTimer객체를 만들고, Interval을 1000으로 설정한 후, ProgrssBar의 값이 늘어나게 하는 함수를 연결하고 QTimer를 시작합니다.
+        #QTimer에 대한 설명은 02.17.01 QTimer에서 보실 수 있습니다.
+        self.timerVar = QTimer()
+        self.timerVar.setInterval(1000)
+        self.timerVar.timeout.connect(self.progressBarTimer)
+        self.timerVar.start()
 
-        reset()
+    def progressBarTimer(self) :
+        self.time = self.progressBar_Test.value()
+        self.time += 1
+        self.progressBar_Test.setValue(self.time)
 
-        value() / setValue()
-        """
+        #ProgressBar의 값이 progressBar의 최댓값 이상이 되면 Timer를 중단시켜 ProgressBar의 값이 더이상 증가하지 않게 합니다.
+        if self.time >= self.progressBar_Test.maximum() :
+            self.timerVar.stop()
 
-
-
+    def printValue(self) :
+        print(self.progressBar_Test.value())
+        
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindow = WindowClass()
